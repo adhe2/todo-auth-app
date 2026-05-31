fd1c585f5b855c2593da6235b52cf4fea3bd0a21
adhe2 <adheranggamhmd@gmail.com>
2026-05-31 23:38:43 +0900

    commit 2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("todo_auth_db", "backend", "Mysql@2889", {
-  host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
